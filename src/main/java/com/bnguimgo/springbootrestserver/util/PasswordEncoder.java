7895e6369b33174c1ca92c9c4502b59278b6b091
Ramm759 <Prozac759>
2023-01-22 15:29:21 +0100

    Ajout classe util.PasswordEncoder

---
+++ src/main/java/com/bnguimgo/springbootrestserver/util/PasswordEncoder.java
@@ -0,0 +1,24 @@
+package com.bnguimgo.springbootrestserver.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Classe permettant de créer des mots de passe hachés pour des besoins de tests
+ *et d'initialisation de la base de données
+ *
+ */
+public class PasswordEncoder {
+
+    private static BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public static void main(String[] args) {
+        bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        String password ="password2";
+        String encodedPassword = bCryptPasswordEncoder.encode(password);
+        System.out.println("Mot de passe en clair : "+password);
+        System.out.println("Mot de passe haché : "+encodedPassword);
+        //Pour vérifier que le mot de passe haché correspond bien au mot de passe initial, il utiliser la méthode bCryptPasswordEncoder.matches(x, y)
+        System.out.println("Le mot de passe est bien haché : "+bCryptPasswordEncoder.matches(password, encodedPassword));
+    }
+
+}
